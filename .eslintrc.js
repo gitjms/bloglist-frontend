16dd475b9ff7b610c9ff636c5d63cd1b38740cdf
gitjms <46410240+gitjms@users.noreply.github.com>
2020-07-10 15:28:38 +0300

    security helmet, eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   },
   "extends": [ 
       "eslint:recommended",
-      "plugin:react/recommended"
+      "plugin:react/recommended",
+      "plugin:security/recommended"
   ],
   "parserOptions": {
       "ecmaFeatures": {
@@ -17,7 +18,7 @@ module.exports = {
       "sourceType": "module"
   },
   "plugins": [
-      "react", "jest", "cypress"
+      "react", "jest", "cypress", "security"
   ],
   "rules": {
       "indent": [
